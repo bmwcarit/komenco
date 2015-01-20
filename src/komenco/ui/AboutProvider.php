2a9b7400e350bd34c111469f173b0d8c99644073
Timo Mueller <timo.mueller@bmw-carit.de>
2015-01-20 09:40:47 +0100

    ui: Add about provider
    The about provider registers routes that will render the applications
    about page. Currently the about page only consists of the packages that
    are used to build the application.

---
+++ src/komenco/ui/AboutProvider.php
@@ -0,0 +1,54 @@
+<?php namespace komenco\ui;
+/*
+ * Copyright (C) 2015, BMW Car IT GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+require_once __DIR__ .  '/../../..' . '/vendor/autoload.php';
+
+use Silex\Application;
+use Silex\ControllerProviderInterface;
+
+class AboutProvider implements ControllerProviderInterface{
+	public function connect(Application $app) {
+		$controllers = $app['controllers_factory'];
+
+		$app['about.template'] = 'about.twig';
+		$app['about.packages.file'] = __DIR__ . '/../../..' .
+										'/vendor/composer/installed.json';
+
+		$controllers->get('/', function (Application $app) {
+			$licenseFile = $app['about.packages.file'];
+			$data = array();
+			if (file_exists($licenseFile)) {
+				$file = file_get_contents($licenseFile);
+				$data = json_decode($file);
+			} else {
+				$app->log('Could not find licenses file at ' .
+							$app['about.packages.file']);
+			}
+
+			return $app->render($app['about.template'],
+								array('packages' => $data));
+		})->bind('about');
+
+		return $controllers;
+	}
+}
