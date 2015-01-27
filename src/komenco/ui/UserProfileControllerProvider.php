2f40bb33e6b3e36c28b66740b3362286be4f18c7
Michael Knapp <michael.knapp@bmw-carit.de>
2015-01-27 14:21:03 +0100

    ui: Add user profile provider
    The user profile provider renders a page containing information about the
    current logged in user.

---
+++ src/komenco/ui/UserProfileControllerProvider.php
@@ -0,0 +1,39 @@
+<?php namespace komenco\ui;
+/*
+ * Copyright (C) 2015, BMW Car IT GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+require_once __DIR__ .  '/../../..' . '/vendor/autoload.php';
+
+use Silex\Application;
+use Silex\ControllerProviderInterface;
+
+class UserProfileControllerProvider implements ControllerProviderInterface{
+	public function connect(Application $app) {
+		$controllers = $app['controllers_factory'];
+
+		$controllers->get('/', function (Application $app) {
+			return $app->render('user_profile.twig');
+		})->bind('user_profile');
+
+		return $controllers;
+	}
+}
